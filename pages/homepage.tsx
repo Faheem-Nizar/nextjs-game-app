2501c92ede42fbfd620cbc076073fc479737bb5a
Faheem-Nizar <85359396+Faheem-Nizar@users.noreply.github.com>
2021-07-23 09:08:35 +0530

    Update homepage.tsx

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -1,18 +1,12 @@
 import 'antd/dist/antd.css';
 import { Carousel } from 'antd';
 import styles from './frontpage.module.css' 
-const contentStyle = {
-    height: '600px',
-    color: '#fff',
-    lineHeight: '130px',
-    textAlign: 'center',
-    background: '#f44',
-  };
+
 export default function Homepage() {
     return (
         <Carousel autoplay>
     <div>
-      <h3 style={contentStyle} >
+      <h3 className={styles.contentStyle} >
         <div className={styles.frontPage}>
           
             WELCOME TO THE GAME SHACK
@@ -21,7 +15,7 @@ export default function Homepage() {
       </h3>
     </div>
    <div>
-      <h3 style={contentStyle}>
+      <h3 className={styles.contentStyle}>
         <div className={styles.frontPage}>
           
             DIZZY YOUR BRAIN WITH SUDOKU
@@ -30,7 +24,7 @@ export default function Homepage() {
       </h3>
     </div>
     <div>
-      <h3 style={contentStyle}>
+      <h3 className={styles.contentStyle}>
         <div className={styles.frontPage}>
           
             COMPETE WITH YOUR FRIENDS IN TIC TAC TOE
@@ -39,7 +33,7 @@ export default function Homepage() {
       </h3>
     </div>
     <div>
-      <h3 style={contentStyle}>
+      <h3 className={styles.contentStyle}>
         <div className={styles.frontPage}>
           
             WHAT ARE YOU WAITING FOR? GET STARTED
